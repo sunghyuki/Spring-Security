8d433afbe016a32fcdae19be7099aec836dbff3e
sunghyuki <sunghyuk1609@gmail.com>
2022-06-27 00:40:12 +0900

    feat : 스프링 시큐리티 연동

---
+++ src/main/java/com/sunghyuki/demospringsecurity/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.sunghyuki.demospringsecurity.config;
+
+import com.sunghyuki.demospringsecurity.account.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .mvcMatchers("/", "/info", "/account/**").permitAll()
+            .mvcMatchers("/admin").hasRole("ADMIN")
+            .anyRequest().authenticated();
+
+        http.formLogin();
+        http.httpBasic();
+    }
+}
