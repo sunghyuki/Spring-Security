9af725cb1305a84ccfa1b46b86059a4b57e85657
sunghyuki <sunghyuk1609@gmail.com>
2022-06-26 11:59:18 +0900

    init

---
+++ src/main/java/com/sunghyuki/demospringsecurity/DemoSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.sunghyuki.demospringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringSecurityApplication.class, args);
+    }
+
+}
