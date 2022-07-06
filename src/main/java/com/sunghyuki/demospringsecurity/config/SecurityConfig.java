71f2e2557dd3aacda3f08e4544789657bfb10804
sunghyuki <sunghyuk1609@gmail.com>
2022-07-06 11:35:33 +0900

    feat : 로그아웃시 리다이렉트 페이지 변경

--- src/main/java/com/sunghyuki/demospringsecurity/config/SecurityConfig.java
+++ src/main/java/com/sunghyuki/demospringsecurity/config/SecurityConfig.java
@@ -42,6 +42,8 @@ public class  SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin();
         http.httpBasic();
 
+        http.logout().logoutSuccessUrl("/");
+
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL );
     }
 }
